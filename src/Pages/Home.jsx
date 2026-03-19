bfb0840c1652d2350755d76cc0b6a82e3c075e19
ssroci <rocissalinas@gmail.com>
2026-03-19 00:14:55 -0300

    carreras-act

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,76 +1,125 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [universidades, setUniversidades] = useState(0);
+  const [carreras, setCarreras] = useState(0);
+
+  useEffect(() => {
+    const uniTarget = 45;
+    const carTarget = 500;
+
+    let uniCount = 0;
+    let carCount = 0;
+
+    const intervalo = setInterval(() => {
+      if (uniCount < uniTarget) {
+        uniCount++;
+        setUniversidades(uniCount);
+      }
+
+      if (carCount < carTarget) {
+        carCount += 10;
+        setCarreras(carCount);
+      }
+
+      if (uniCount >= uniTarget && carCount >= carTarget) {
+        clearInterval(intervalo);
+      }
+    }, 30);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
   return (
     <div className="home">
 
-      <h1>
-        Guía de carreras <span className="argentina">Argentina</span>
-      </h1>
+      {/* HERO */}
+      <div className="hero">
+        <h1>
+  <span className="titulo-principal">Guía de carreras</span>
+  <span className="argentina">Argentina</span>
+</h1>
 
-      <p className="subtitulo">
-       Esta guía reúne información sobre carreras universitarias y universidades de Argentina. Explora qué estudiar, descubre en qué instituciones se dicta cada carrera y encuentra distintas modalidades de formación para elegir el camino académico que mejor se adapte a ti.
-      </p>
+        <p className="subtitulo">
+          Esta guía reúne información sobre carreras universitarias y universidades de Argentina.
+          Explora qué estudiar, descubre en qué instituciones se dicta cada carrera y encuentra
+          distintas modalidades de formación.
+        </p>
+      </div>
 
+      {/* STATS */}
+      <div className="stats">
+        <div className="stat">
+          <h2>+{universidades}</h2>
+          <p>Universidades</p>
+        </div>
 
-      {/* UNIVERSIDADES */}
-
-      <div className="seccion">
-        <h2 className="seccion-titulo">Universidades</h2>
-
-        <div className="tipos">
-
-          <Link to="/universidades">
-            <button className="universidades">
-              <i className="bi bi-mortarboard-fill"></i>
-              Universidades en Argentina
-            </button>
-          </Link>
-
+        <div className="stat">
+          <h2>+{carreras}</h2>
+          <p>Carreras</p>
         </div>
       </div>
 
+      {/* UNIVERSIDADES */}
+      <div className="seccion">
+        <h2 className="seccion-titulo">Universidades</h2>
+      
+        <div className="tipos">
+          <Link to="/universidades" className="card-link">
+            <div className="card universidades">
+              <div className="card-circle">
+                <i className="bi bi-mortarboard-fill"></i>
+              </div>
+              <h3>Universidades de Argentina </h3>
+             <p> Explorá las universidades y elegí la carrera de tus sueños.</p>
+            </div>
+          </Link>
+        </div>
+      </div>
 
       {/* CARRERAS */}
-
       <div className="seccion">
         <h2 className="seccion-titulo">Carreras</h2>
 
         <div className="tipos">
-
-          <Link to="/presencial">
-            <button className="presencial">
-              <i className="bi bi-person-fill"></i>
-              Presencial
-            </button>
+          <Link to="/presencial" className="card-link">
+            <div className="card presencial">
+              <div className="card-circle">
+                <i className="bi bi-person-fill"></i>
+              </div>
+              <h3>Presencial</h3>
+              <p>Carreras en modalidad presencial</p>
+            </div>
           </Link>
 
-          <Link to="/virtual">
-            <button className="distancia">
-              <i className="bi bi-pc-display-horizontal"></i>
-              A distancia
-            </button>
+          <Link to="/virtual" className="card-link">
+            <div className="card distancia">
+              <div className="card-circle">
+                <i className="bi bi-pc-display-horizontal"></i>
+              </div>
+              <h3>A distancia</h3>
+              <p>Carreras online</p>
+            </div>
           </Link>
-
         </div>
       </div>
 
-
       {/* CURSOS */}
-
       <div className="seccion">
         <h2 className="seccion-titulo">Cursos</h2>
 
         <div className="tipos">
-
-          <Link to="/cursos">
-            <button className="cursos">
-              <i className="bi bi-book"></i>
-              Cursos / Capacitaciones
-            </button>
+          <Link to="/cursos" className="card-link">
+            <div className="card cursos">
+              <div className="card-circle">
+                <i className="bi bi-book"></i>
+              </div>
+              <h3>Cursos</h3>
+              <p>Capacitaciones y formación corta</p>
+            </div>
           </Link>
-
         </div>
       </div>
 
--- src/Pages/UniversidadesDetalle.jsx
+++ src/Pages/UniversidadesDetalle.jsx
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+
 import { carrerasUNRN } from "../Data/CarrerasUni/unrndata";
 import { carrerasUNCOMA } from "../Data/CarrerasUni/uncomadata";
 import { carrerasUNLP } from "../Data/CarrerasUni/unlpdata";
@@ -17,7 +19,6 @@ import { carrerasUNCUYO } from "../Data/CarrerasUni/uncuyo";
 import { carrerasUNaM } from "../Data/CarrerasUni/unam";
 import { carrerasUNSa } from "../Data/CarrerasUni/unsa";
 import { carrerasUNCA } from "../Data/CarrerasUni/unca";
-import { carrerasUNF } from "../Data/CarrerasUni/unf";
 import { carrerasUNSJ } from "../Data/CarrerasUni/unsjdata";
 import { carrerasUNTDF } from "../Data/CarrerasUni/untdfdata";
 import { carrerasUNSE } from "../Data/CarrerasUni/unsedata";
@@ -25,7 +26,6 @@ import { carrerasUNT } from "../Data/CarrerasUni/untdata";
 import { carrerasUNLaR } from "../Data/CarrerasUni/unlardata";
 import { carrerasUNO } from "../Data/CarrerasUni/unodata";
 import { carrerasUNMDP } from "../Data/CarrerasUni/mdqdata";
-import { carrerasUNDEC } from "../Data/CarrerasUni/undecdata";
 import { carrerasUNQ } from "../Data/CarrerasUni/unqdata";
 import { carrerasUNLAM } from "../Data/CarrerasUni/unlamdata";
 import { carrerasUNLPAM } from "../Data/CarrerasUni/unlapdata";
@@ -39,7 +39,6 @@ import { carrerasUNM } from "../Data/CarrerasUni/unmoredata";
 import { carrerasUNAHUR } from "../Data/CarrerasUni/unhurdata";
 import { carrerasUNPAZ } from "../Data/CarrerasUni/unpazdata";
 import { carrerasUNRAF } from "../Data/CarrerasUni/unrafdata";
-import { carrerasUNSAdA } from "../Data/CarrerasUni/unsadadata";
 import { carrerasUNTREF } from "../Data/CarrerasUni/untrefdata";
 import { carrerasUNVM } from "../Data/CarrerasUni/unvmdata";
 import { carrerasUNViMe } from "../Data/CarrerasUni/unvimedata";
@@ -48,138 +47,112 @@ import { carrerasUNL } from "../Data/CarrerasUni/unlidata";
 import { carrerasUNNOBA } from "../Data/CarrerasUni/unnobadata";
 import { carrerasUNIPE } from "../Data/CarrerasUni/unipedata";
 import { carrerasUNICEN } from "../Data/CarrerasUni/unicendata";
+import { carrerasUNDEC } from "../Data/CarrerasUni/undecdata";
+import { carrerasUNF } from "../Data/CarrerasUni/unf";
+import {carrerasUNSAdA} from "../Data/CarrerasUni/unsadadata"
 
+import "./UniversidadesDetalle.css";
 
 function UniversidadesDetalle() {
   const { id } = useParams();
+  const [busqueda, setBusqueda] = useState("");
 
-  let carreras = [];
+  const universidadesData = {
+    unrn: { nombre: "Universidad Nacional de Río Negro", carreras: carrerasUNRN },
+    uncoma: { nombre: "Universidad Nacional del Comahue", carreras: carrerasUNCOMA },
+    unlp: { nombre: "Universidad Nacional de La Plata", carreras: carrerasUNLP },
+    uba: { nombre: "Universidad de Buenos Aires", carreras: carrerasUBA },
+    unc: { nombre: "Universidad Nacional de Córdoba", carreras: carrerasUNC },
+    unca: { nombre: "Universidad Nacional de Catamarca", carreras: carrerasUNCA },
+    unsa: { nombre: "Universidad Nacional de Salta", carreras: carrerasUNSa },
+    unsj: { nombre: "Universidad Nacional de San Juan", carreras: carrerasUNSJ },
+    unch: { nombre: "Universidad Nacional de Chilecito", carreras: carrerasUNDEC },
+    uncuyo: { nombre: "Universidad Nacional de Cuyo", carreras: carrerasUNCUYO },
+    uner: { nombre: "Universidad Nacional de Entre Ríos", carreras: carrerasUNER },
+    unlu: { nombre: "Universidad Nacional de Luján", carreras: carrerasUNLU },
+    unmdp: { nombre: "Universidad Nacional de Mar del Plata", carreras: carrerasUNMDP },
+    unlam: { nombre: "Universidad Nacional de La Matanza", carreras: carrerasUNLAM },
+    unla: { nombre: "Universidad Nacional de Lanús", carreras: carrerasUNLA },
+    unlz: { nombre: "Universidad Nacional de Lomas de Zamora", carreras: carrerasUNLZ },
+    unq: { nombre: "Universidad Nacional de Quilmes", carreras: carrerasUNQ },
+    unr: { nombre: "Universidad Nacional de Rosario", carreras: carrerasUNRO },
+    unrc: { nombre: "Universidad Nacional de Río Cuarto", carreras: carrerasUNRC },
+    unse: { nombre: "Universidad Nacional de Santiago del Estero", carreras: carrerasUNSE },
+    unt: { nombre: "Universidad Nacional de Tucumán", carreras: carrerasUNT },
+    unvm: { nombre: "Universidad Nacional de Villa María", carreras: carrerasUNVM },
+    unraf: { nombre: "Universidad Nacional de Rafaela", carreras: carrerasUNRAF },
+    undav: { nombre: "Universidad Nacional de Avellaneda", carreras: carrerasUNDAV },
+    unicen: { nombre: "Universidad Nacional del Centro", carreras: carrerasUNICEN },
+    unl: { nombre: "Universidad Nacional del Litoral", carreras: carrerasUNL },
+    unnoba: { nombre: "Universidad Nacional del Noroeste de Buenos Aires", carreras: carrerasUNNOBA },
+    uno: { nombre: "Universidad Nacional del Oeste", carreras: carrerasUNO },
+    unpaz: { nombre: "Universidad Nacional de José C. Paz", carreras: carrerasUNPAZ },
+    unahur: { nombre: "Universidad Nacional de Hurlingham", carreras: carrerasUNAHUR },
+    unsam: { nombre: "Universidad Nacional de San Martín", carreras: carrerasUNSAM },
+    ungs: { nombre: "Universidad Nacional de General Sarmiento", carreras: carrerasUNGS },
+    unm: { nombre: "Universidad Nacional de Moreno", carreras: carrerasUNM },
+    unju: { nombre: "Universidad Nacional de Jujuy", carreras: carrerasUNJU },
+    una: { nombre: "Universidad Nacional de las Artes", carreras: carrerasUNA },
+    unlar: { nombre: "Universidad Nacional de La Rioja", carreras: carrerasUNLaR },
+    unlpam: { nombre: "Universidad Nacional de La Pampa", carreras: carrerasUNLPAM },
+    unam: { nombre: "Universidad Nacional de Misiones", carreras: carrerasUNaM },
+    untref: { nombre: "Universidad Nacional de Tres de Febrero", carreras: carrerasUNTREF },
+    untf: { nombre: "Universidad Nacional de Tierra del Fuego", carreras: carrerasUNTDF },
+    unvime: { nombre: "Universidad Nacional de Villa Mercedes", carreras: carrerasUNViMe },
+    unipe: { nombre: "Universidad Pedagógica Nacional", carreras: carrerasUNIPE },
+    unsa: { nombre: "Universidad Nacional  de Salta", carreras: carrerasUNSa },
+    uflo: { nombre: "Universidad de Flores", carreras: carrerasUFLO },
+    siglo21: { nombre: "Universidad Siglo 21", carreras: carrerasSiglo21 },
+    um: { nombre: "Universidad de Mendoza", carreras: carrerasUM },
+     unsl: { nombre: "Universidad Nacional de San Luis", carreras: carrerasUNSL },
+     ucasal: { nombre: "Universidad Católica de Salta", carreras: carrerasUCASAL },
+     unf: { nombre: "Universidad Nacional de Formosa", carreras: carrerasUNF },
+    unsada: { nombre: "Universidad Nacional de San Antonio de Areco", carreras: carrerasUNSAdA },
+  };
 
-  if (id === "unrn") {
-    carreras = carrerasUNRN;
-  } else if (id === "uncoma") {
-    carreras = carrerasUNCOMA;
-  } else if (id === "unlp") {
-    carreras = carrerasUNLP; 
-  } else if (id === "uba") {
-    carreras = carrerasUBA;
-  } else if (id === "unc") {
-    carreras = carrerasUNC;
-    } else if (id === "unrc") {
-    carreras = carrerasUNRC;
-      } else if (id === "unc") {
-    carreras = carrerasUNC;
-   } else if (id === "ucasal") {
-    carreras = carrerasUCASAL;
-     } else if (id === "unlu") {
-    carreras = carrerasUNLU;
-     } else if (id === "uflo") {
-    carreras = carrerasUFLO;
-     } else if (id === "unr") {
-    carreras = carrerasUNRO;
-     } else if (id === "um") {
-    carreras = carrerasUM;
-      } else if (id === "unsl") {
-    carreras = carrerasUNSL;
-       } else if (id === "siglo21") {
-    carreras = carrerasSiglo21;
-      } else if (id === "uner") {
-    carreras = carrerasUNER;
-      } else if (id === "uncuyo") {
-    carreras = carrerasUNCUYO;
-      } else if (id === "unam") {
-    carreras = carrerasUNaM;
-      } else if (id === "unsa") {
-    carreras = carrerasUNSa;
-     } else if (id === "unca") {
-    carreras = carrerasUNCA;
-     } else if (id === "unf") {
-    carreras = carrerasUNF;
-     } else if (id === "unsj") {
-    carreras = carrerasUNSJ;
-       } else if (id === "untdf") {
-    carreras = carrerasUNTDF;
-     } else if (id === "unse") {
-    carreras = carrerasUNSE;
-      } else if (id === "unt") {
-    carreras = carrerasUNT;
-     } else if (id === "unlar") {
-    carreras = carrerasUNLaR;
-      } else if (id === "uno") {
-    carreras = carrerasUNO;
-      } else if (id === "unmdp") {
-    carreras = carrerasUNMDP;
-        } else if (id === "unch") {
-    carreras = carrerasUNDEC;
-    } else if (id === "unq") {
-    carreras = carrerasUNQ;
-        } else if (id === "unlam") {
-    carreras = carrerasUNLAM;
-    } else if (id === "unlpam") {
-    carreras = carrerasUNLPAM;
-} else if (id === "unla") {
-    carreras = carrerasUNLA;
-    } else if (id === "unju") {
-    carreras = carrerasUNJU;
-  } else if (id === "una") {
-    carreras = carrerasUNA;
-   } else if (id === "unlz") {
-    carreras = carrerasUNLZ;
-       } else if (id === "unsam") {
-    carreras = carrerasUNSAM;
-      } else if (id === "ungs") {
-    carreras = carrerasUNGS;
-  } else if (id === "unm") {
-    carreras = carrerasUNM;
-     } else if (id === "unahur") {
-    carreras = carrerasUNAHUR;
-   } else if (id === "unpaz") {
-    carreras = carrerasUNPAZ;
-  } else if (id === "unraf") {
-    carreras = carrerasUNRAF;
-    } else if (id === "unsada") {
-    carreras = carrerasUNSAdA;
-  } else if (id === "untref") {
-    carreras = carrerasUNTREF;
-   } else if (id === "unvm") {
-    carreras = carrerasUNVM;
-     } else if (id === "unvime") {
-    carreras = carrerasUNViMe;
-     } else if (id === "undav") {
-    carreras = carrerasUNDAV;
-    } else if (id === "unl") {
-    carreras = carrerasUNL;
-    } else if (id === "unnoba") {
-    carreras = carrerasUNNOBA;
-  } else if (id === "unipe") {
-    carreras = carrerasUNIPE;
+  const universidad = universidadesData[id];
+  const carreras = universidad ? universidad.carreras : [];
+  const nombreUni = universidad ? universidad.nombre : "Universidad";
 
-      } else if (id === "unicen") {
-    carreras = carrerasUNICEN;
-
-
-
-
-
-
-    
-  }
+  const carrerasFiltradas = carreras.filter((carrera) =>
+    carrera.toLowerCase().includes(busqueda.toLowerCase())
+  );
 
   return (
-    <div>
-      <h1>Carreras</h1>
+    <div className="container">
+<div className="filtro">
+  <h1 className="titulo">{nombreUni}</h1>
 
-      {carreras.length === 0 ? (
-        <p>No hay carreras disponibles.</p>
+  <input
+    type="text"
+    placeholder="Buscar carrera..."
+    value={busqueda}
+    onChange={(e) => setBusqueda(e.target.value)}
+    className="buscador"
+  />
+
+  <p className="resultados">
+    {carrerasFiltradas.length} resultados
+  </p>
+</div>
+    
+      {carrerasFiltradas.length === 0 ? (
+        <p className="sin-resultados">No se encontraron carreras.</p>
       ) : (
-        <ul>
-          {carreras.map((carrera, index) => {
+        <ul className="lista">
+          {carrerasFiltradas.map((carrera, index) => {
             const [texto, link] = carrera.split("|");
 
             return (
-              <li key={index}>
-                {texto}{" "}
-                <a href={link} target="_blank" rel="noopener noreferrer">
+              <li key={index} className="item">
+                <span className="nombre-carrera">{texto}</span>
+
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="boton"
+                >
                   Ver carrera
                 </a>
               </li>
